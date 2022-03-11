ddad7f457e70348d82c36e5f66d030105ede5ee4
levant12 <100776976+levant12@users.noreply.github.com>
2022-03-11 20:56:22 +0400

    validation

--- assests/js/add.js
+++ assests/js/add.js
@@ -1,3 +1,7 @@
+const saveBtn = document.getElementById("save-btn");
+const prodType = document.getElementById("productType");
+const formChildNodes = document.getElementById("addProduct").childNodes;
+
 // set dropdown menu default value
 $(document).ready(function () {
   $("#productType").val("dvd");
@@ -8,19 +12,21 @@ const products = {
   furniture: {
     value: "furniture",
     elementId: "fInfo",
+    descriptionId: "height width lenght",
   },
   book: {
     value: "book",
     elementId: "bInfo",
+    descriptionId: "weight",
   },
   dvd: {
     value: "dvd",
     elementId: "dInfo",
+    descriptionId: "size",
   },
 };
 
 const changeForm = () => {
-  const prodType = document.getElementById("productType");
   const type = prodType.options[prodType.selectedIndex].value;
 
   // manipulate ui after
@@ -38,3 +44,78 @@ const changeForm = () => {
     }
   }
 };
+
+// validate description
+const checkDescr = (ids) => {
+  for (let i = 0; i < ids.length; i++) {
+    if (document.getElementById(ids[i]).value === "") {
+      return true;
+    }
+  }
+  return false;
+};
+
+const emptyInput = () => {
+  // get elements from html
+  const sku = document.getElementById("sku");
+  const name = document.getElementById("name");
+  const price = document.getElementById("price");
+  const type = prodType.options[prodType.selectedIndex].value;
+  const productValueObject = products[type];
+  const ids = productValueObject.descriptionId.split(" ");
+  if (
+    sku.value === "" ||
+    name.value === "" ||
+    price.value === "" ||
+    checkDescr(ids)
+  ) {
+    return true;
+  }
+  return false;
+};
+
+const invalidSKU = () => {
+  const sku = document.getElementById("sku");
+  var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  if (format.test(sku.value)) return true;
+  return false;
+};
+
+// validate input
+saveBtn.addEventListener("click", function (e) {
+  if (emptyInput()) {
+    e.preventDefault();
+    // create new label for warning text
+    var label = document.createElement("label");
+    label.htmlFor = "warning";
+    label.innerHTML = "Please fill every field";
+    label.classList.add("warning");
+    // check if user is already warned
+    if (
+      formChildNodes[formChildNodes.length - 1].tagName != "LABEL" ||
+      formChildNodes[formChildNodes.length - 1].textContent ===
+        "Please provide SKU without symbols"
+    ) {
+      document
+        .getElementById("addProduct")
+        .replaceChild(label, formChildNodes[formChildNodes.length - 1]);
+    }
+  } else if (invalidSKU()) {
+    e.preventDefault();
+    // create new label for warning text
+    var label = document.createElement("label");
+    label.htmlFor = "warning";
+    label.innerHTML = "Please provide SKU without symbols";
+    label.classList.add("warning");
+    // check if user is already warned
+    if (
+      formChildNodes[formChildNodes.length - 1].tagName != "LABEL" ||
+      formChildNodes[formChildNodes.length - 1].textContent ===
+        "Please fill every field"
+    ) {
+      document
+        .getElementById("addProduct")
+        .replaceChild(label, formChildNodes[formChildNodes.length - 1]);
+    }
+  }
+});
